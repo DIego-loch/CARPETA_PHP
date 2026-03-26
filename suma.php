c6fb28b14bd2cd6c32d1c5beae56cac95f496bbc
diego_loch <cout3728@gmail.com>
2026-03-26 12:33:58 -0600

    agrego el ejercicio de suma

---
+++ suma.php
@@ -0,0 +1,9 @@
+<?php
+echo "Ingrese su primer numero\n";
+$numero1 = readline();
+echo "Ingrese su segundo numero\n";
+$numero2 = readline();
+
+$suma = $numero1 + $numero2;
+echo "La suma de sus valores es: " . $suma . "\n";
+?>
